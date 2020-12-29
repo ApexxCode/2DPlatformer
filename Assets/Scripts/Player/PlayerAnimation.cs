85117537df40c7d4023d651b7b88f871d4cc2093
Apexx <Apexxcode@protonmail.com>
2020-12-29 14:59:26 -0500

    Reverted back a few steps to re-work Player movement and animations

--- Assets/Scripts/Player/PlayerAnimation.cs
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -4,18 +4,5 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    private Animator theAnim;
 
-    private void Awake()
-    {
-        theAnim = GetComponentInChildren<Animator>();
-    }
-
-    public void Move(float move)
-    {
-        if (theAnim != null)
-        {
-            theAnim.SetFloat("move", Mathf.Abs(move));
-        }
-    }
 }
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,129 +6,59 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [Header("Player Physics")]
-    [SerializeField, Tooltip("How much time is allowed when the player is over a ledge to allow a jump.")] private float hangTime = 0.2f;
-    [SerializeField, Tooltip("How much time is allowed to enable jump, before hitting the ground.")] private float jumpBufferTime = 0.1f;
+    //[SerializeField, Tooltip("How much time is allowed when the player is over a ledge to allow a jump.")] private float hangTime = 0.2f;
+    //[SerializeField, Tooltip("How much time is allowed to enable jump, before hitting the ground.")] private float jumpBufferTime = 0.1f;
     [SerializeField, Tooltip("How much force to apply to the Player's vertical position when jumping.")] private float jumpVelocity = 12f;
     [SerializeField, Tooltip("How much force to apply the the Player's horizontal position.")] private float moveVelocity = 4f;
     [SerializeField, Tooltip("Select or Create a Layer designated for the ground.")] private LayerMask groundLayer;
 
     [Header("Player Effects")]
-    [SerializeField, Tooltip("The ParticleSystem you would like for footsteps.")] private ParticleSystem footstepParticles;
+    [SerializeField, Tooltip("The ParticleSystem you would like for footsteps.")] private ParticleSystem _footstepParticles;
     [SerializeField, Tooltip("The amount of particles to emmit over distance."), Range(0.1f, 10.0f)] private float particlesOverDistance = 1f;
 
-    private PlayerAnimation theAnimator;
-    private BoxCollider2D theBoxCollider;
-    private ParticleSystem.EmissionModule footEmission;
-    private PlayRandomSounds playRandomSounds;
-    private Rigidbody2D theRB;
-    private bool facingRight = true;
+    private BoxCollider2D _boxCollider;
+    private ParticleSystem.EmissionModule _footEmission;
+    private PlayRandomSounds _playeRandomSounds;
+    private Rigidbody2D _rigid;
+    private bool facingRight = true, jumping, resetJump;
     private float hangCounter;
-    private float move;
+    private float horizontalInput;
     private float jumpBufferCount;
 
     private void Awake()
     {
-        footstepParticles = GetComponentInChildren<ParticleSystem>();
-        theAnimator = GetComponent<PlayerAnimation>();
-        theRB = GetComponent<Rigidbody2D>();
-        theBoxCollider = GetComponent<BoxCollider2D>();
-        footEmission = footstepParticles.emission;
-        playRandomSounds = GetComponent<PlayRandomSounds>();
+        _footstepParticles = GetComponentInChildren<ParticleSystem>();
+        _rigid = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
+        _footEmission = _footstepParticles.emission;
+        _playeRandomSounds = GetComponent<PlayRandomSounds>();
 
-        #region Error Checks
-
-        if (footstepParticles == null)
-            Debug.LogError($"You are missing a ParticleSystem component on the Player object.");
-        if (theAnimator == null)
-            Debug.LogError($"You are missing an Animator component on the Player object.");
-        if (theRB == null)
-            Debug.LogError($"You are missing a Rigidbody2D component on the Player object.");
-        if (theBoxCollider == null)
-            Debug.LogError($"You are missing a BoxCollider2D component on the Player object.");
-        if (theRB == null)
-            Debug.LogError($"You are missing a Rigidbody2D component on the Player object.");
-        #endregion
+        _rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     private void Update()
     {
-        //Update the Animator controller with the player's grounded state
-        //theAnimator.SetBool("grounded", IsGrounded());
-
-        Movement();
+        CheckUserInput();
         PlayFootsteps();
-    }
-
-    private void PlayFootsteps()
-    {
-        //Footstep particles when grounded and moving either right or left
-        if (IsGrounded() && Input.GetAxisRaw("Horizontal") != 0)
-        {
-            footEmission.rateOverDistance = particlesOverDistance;
-            playRandomSounds.PlaySound();
-        }
-        else
-        {
-            footEmission.rateOverDistance = 0;
-        }
     }
 
     private void FixedUpdate()
     {
         //All physics code should be handled inside FixedUpdate()
-
-        theRB.constraints = RigidbodyConstraints2D.FreezeRotation;
-
-        //Manage Hang-time
-        if (IsGrounded() && theRB.velocity.y == 0)
-        {
-            hangCounter = hangTime;
-            //Debug.Log($"hangCounter={hangCounter}");
-        }
-        else
-        {
-            hangCounter -= Time.deltaTime;
-            //Debug.Log($"hangCounter={hangCounter}");
-        }
-
-        //Move the player
-        theAnimator.Move(move);
-        theRB.velocity = new Vector2(move * moveVelocity, theRB.velocity.y);
+        Movement();
     }
 
-    private void Movement()
+    private void CheckUserInput()
     {
         //Store horizontal input from the player
-        move = Input.GetAxisRaw("Horizontal");
+        horizontalInput = Input.GetAxisRaw("Horizontal");
 
-        if (move > 0 && !facingRight || move < 0 && facingRight)
+        if (horizontalInput > 0 && !facingRight || horizontalInput < 0 && facingRight)
             Flip();
 
-        //Manage jump buffer
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            //Jump button was pressed
-            jumpBufferCount = jumpBufferTime;
-        }
-        else
-        {
-            jumpBufferCount -= Time.deltaTime;
-        }
-
-        //Check managed Hang-time and jump buffer before modifying the velocity for Jump
-        if (jumpBufferCount >= 0 && hangCounter > 0)
-        {
-            theRB.velocity = Vector2.up * jumpVelocity;
-            //Reset counters
-            jumpBufferCount = 0;
-            hangCounter = 0;
-        }
-
-        //If the Jump button was released and Player is moving up
-        if (Input.GetButtonUp("Jump") && theRB.velocity.y > 0) 
-        {
-            //Cut the player's y-velocity in half
-            theRB.velocity = new Vector2(theRB.velocity.x, theRB.velocity.y * 0.5f);
+            jumping = true;
         }
 
         //R Key was pressed - Reset Player position to home (0, 0, 0)
@@ -138,12 +68,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void PlayFootsteps()
+    {
+        //Footstep particles when grounded and moving either right or left
+        if (IsGrounded() && Input.GetAxisRaw("Horizontal") != 0)
+        {
+            _footEmission.rateOverDistance = particlesOverDistance;
+            _playeRandomSounds.PlaySound();
+        }
+        else
+        {
+            _footEmission.rateOverDistance = 0;
+        }
+    }
+
+    private void Movement()
+    {
+        if (jumping)
+        {
+            _rigid.velocity = Vector2.up * jumpVelocity;
+            jumping = false;
+        }
+
+        //Move the player
+        _rigid.velocity = new Vector2(horizontalInput * moveVelocity, _rigid.velocity.y);
+    }
+
     private bool IsGrounded()
     {
         float extraHeight = 0.1f;
         Color rayColor;
 
-        RaycastHit2D raycastHit = Physics2D.BoxCast(theBoxCollider.bounds.center, theBoxCollider.bounds.size, 0f, Vector2.down, extraHeight, groundLayer);
+        RaycastHit2D raycastHit = Physics2D.BoxCast(_boxCollider.bounds.center, _boxCollider.bounds.size, 0f, Vector2.down, extraHeight, groundLayer);
         
         //If hit something...
         if (raycastHit.collider != null)
@@ -155,9 +111,9 @@ public class PlayerController : MonoBehaviour
             rayColor = Color.red;
         }
         
-        Debug.DrawRay(theBoxCollider.bounds.center + new Vector3(theBoxCollider.bounds.extents.x, 0), Vector2.down * (theBoxCollider.bounds.extents.y + extraHeight), rayColor);
-        Debug.DrawRay(theBoxCollider.bounds.center - new Vector3(theBoxCollider.bounds.extents.x, 0), Vector2.down * (theBoxCollider.bounds.extents.y + extraHeight), rayColor);
-        Debug.DrawRay(theBoxCollider.bounds.center - new Vector3(theBoxCollider.bounds.extents.x, theBoxCollider.bounds.extents.y), (Vector2.right * theBoxCollider.bounds.extents.x) * 2, rayColor);
+        Debug.DrawRay(_boxCollider.bounds.center + new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + extraHeight), rayColor);
+        Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, 0), Vector2.down * (_boxCollider.bounds.extents.y + extraHeight), rayColor);
+        Debug.DrawRay(_boxCollider.bounds.center - new Vector3(_boxCollider.bounds.extents.x, _boxCollider.bounds.extents.y), (Vector2.right * _boxCollider.bounds.extents.x) * 2, rayColor);
         //Debug.Log(raycastHit.collider);
 
         return raycastHit.collider != null;
